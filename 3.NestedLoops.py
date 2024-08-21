096136e1eb9fe0e68f0bbbf1256397b678506f60
Jake Maloney <neptunesubs85@gmail.com>
2024-08-21 10:52:35 +1000

    added features

---
+++ 3.NestedLoops.py
@@ -0,0 +1,9 @@
+# Using Nested Loops basically means adding one loop inside of another loop, for example we can generate a list of coordinates
+# for x in range(4):
+#     for y in range(3):
+#         print(f"({x}, {yuuiugiuh})")
+
+# Exercise
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    print('x' * x_count)
