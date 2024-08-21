3480ce87bb797f80fc19e5caa89bcc71bde97801
Jake Maloney <neptunesubs85@gmail.com>
2024-08-21 10:44:43 +1000

    second commit

---
+++ 2.ForLoops.py
@@ -0,0 +1,9 @@
+# We use For Loops to iterate over items of a collection, such as a string
+# for item in range(5, 10, 2):
+#     print(item)
+
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+    total += price 
+print(f"Total: {total}")
